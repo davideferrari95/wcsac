b5dc9cc19d9f8fa9347387556f41d0a14dcf5823
davideferrari95 <davideferr@unimore.it>
2023-01-11 23:33:00 +0100

    Render Mode Fix
    New version of gym requires to specify the render mode in the gym.make
    function instead of in the env.render function

--- video.py
+++ video.py
@@ -20,8 +20,7 @@ class VideoRecorder(object):
 
     def record(self, env):
         if self.enabled:
-            frame = env.render(mode='rgb_array',
-                               height=self.height,
+            frame = env.render(height=self.height,
                                width=self.width,
                                camera_id=self.camera_id)
             self.frames.append(frame)
